f2518ea83f2b55983e7e2da4eddfd2890699f071
Andromeda252 <122303632+Andromeda252@users.noreply.github.com>
2024-09-23 22:17:42 -0400

    Update SMTPmailClient.py

--- SMTPmailClient.py
+++ SMTPmailClient.py
@@ -5,10 +5,12 @@ endmsg = "\r\n.\r\n"
 
 # Choose a mail server (e.g. Google mail server) and call it mailserver
 mailserver = 'list.winthrop.edu'
+servername = 'localhost'
+serverport = 12000
 
 # Create socket called clientSocket and establish a TCP connection with mailserver
 clientSocket = socket(AF_INET,SOCK_STREAM)
-clientSocket.connect((mailserver, 12000))
+clientSocket.connect(('localhost', 12000))
 recv = clientSocket.recv(1024).decode()
 print(recv)
 if recv[:3] != '220':
